99a19bdd2783dceca85c6023b6a71f5d72a446fa
Yicheng Liu <liuyicheng1991@hotmail.com>
2016-09-26 13:58:04 +0800

    basic merge

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # http-server
 This repository implements HTTP server 1.1 with pure Linux apis.
 
-This project referred to <http://blog.csdn.net/ruizeng88/article/details/6682028>
+This project referred to 
+
+[1] http://blog.csdn.net/ruizeng88/article/details/6682028
+
+[2] https://segmentfault.com/a/1190000003063859
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define BUF_LEN 1028
-#define SERVER_PORT 8080
+#define PORT 8080
+#define IPADDRESS   "127.0.0.1"
+#define MAXSIZE     1024
+#define LISTENQ     5
+#define FDSIZE      15000
+#define EPOLLEVENTS 100
 
 //定义好的html页面，实际情况下web server基本是从本地文件系统读取html文件
 const static char http_error_hdr[] = "HTTP/1.1 404 Not Found\r\nContent-type: text/html\r\n\r\n";
@@ -52,8 +60,105 @@ void serve(int sockfd){
     }
 }
 
+
+
+
+//写处理
+static void do_write(int epollfd,int fd,char *buf) {
+    int nwrite;
+    nwrite = write(fd,buf,strlen(buf));
+    if (nwrite == -1){
+        perror("write error:");
+        close(fd);   //记住close fd
+        struct epoll_event tmp;
+        tmp.events = EPOLLOUT;
+        epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &tmp);
+    }else{
+        struct epoll_event tmp;
+        tmp.events = EPOLLIN;
+        epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &tmp);
+    }
+    memset(buf,0,MAXSIZE);
+}
+
+static void add_event(int epollfd,int fd,int state){
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+}
+
+//删除事件
+static void delete_event(int epollfd,int fd,int state) {
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,&ev);
+}
+
+//修改事件
+static void modify_event(int epollfd,int fd,int state){
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&ev);
+}
+
+static void do_read(int epollfd,int fd,char *buf){
+    int nread;
+    nread = read(fd,buf,MAXSIZE);
+    if (nread == -1)     {
+        perror("read error:");
+        close(fd); //记住close fd
+        delete_event(epollfd,fd,EPOLLIN); //删除监听
+    }
+    else if (nread == 0)     {
+        fprintf(stderr,"client close.\n");
+        close(fd); //记住close fd
+        delete_event(epollfd,fd,EPOLLIN); //删除监听
+    }
+    else {
+        printf("read message is : %s",buf);
+        //修改描述符对应的事件，由读改为写
+        modify_event(epollfd,fd,EPOLLOUT);
+    }
+}
+
+//处理接收到的连接
+static void handle_accpet(int epollfd,int listenfd){
+    int clifd;
+    struct sockaddr_in cliaddr;
+    socklen_t  cliaddrlen;
+    clifd = accept(listenfd,(struct sockaddr*)&cliaddr,&cliaddrlen);
+    if (clifd == -1)
+        perror("accpet error:");
+    else {
+        printf("accept a new client: %s:%d\n",inet_ntoa(cliaddr.sin_addr),cliaddr.sin_port);                       //添加一个客户描述符和事件
+        add_event(epollfd,clifd,EPOLLIN);
+    }
+}
+
+//事件处理函数
+static void handle_events(int epollfd,struct epoll_event *events,int num,int listenfd,char *buf)
+{
+    int i;
+    int fd;
+    //进行遍历;这里只要遍历已经准备好的io事件。num并不是当初epoll_create时的FDSIZE。
+    for (i = 0;i < num;i++)
+    {
+        fd = events[i].data.fd;
+        //根据描述符的类型和事件类型进行处理
+        if ((fd == listenfd) &&(events[i].events & EPOLLIN))
+            handle_accpet(epollfd,listenfd);
+        else if (events[i].events & EPOLLIN)
+            do_read(epollfd,fd,buf);
+        else if (events[i].events & EPOLLOUT)
+            do_write(epollfd,fd,buf);
+    }
+}
+
 int main() {
-    int sockfd, err, newfd;
+    int sockfd, err, newfd, epollfd;
     struct sockaddr_in addr;
     //建立TCP套接字
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -65,18 +170,39 @@ int main() {
     addr.sin_family = AF_INET;
     //这里要注意，端口号一定要使用htons先转化为网络字节序，否则绑定的实际端口
     //可能和你需要的不同
-    addr.sin_port = htons(SERVER_PORT);
+    addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
     if (bind(sockfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in))) {
         perror("socket binding failed!\n");
         return -2;
     }
-    listen(sockfd, 128);
-    for (;;) {
-        //不间断接收HTTP请求并处理，这里使用单线程，在实际情况下考虑到效率一般多线程
-        newfd = accept(sockfd, NULL, NULL);
-        serve(newfd);
-        close(newfd);
+
+    // I am not sure whether the following part is needed.
+    if (!listen(sockfd, 128)) {
+        perror("prepare listening failed\n");
+        return -3;
+    };
+
+    struct epoll_event events[EPOLLEVENTS];
+
+    epollfd = epoll_create(FDSIZE);
+
+    add_event(epollfd, sockfd, EPOLLIN);
+
+//循环等待
+    for ( ; ; ){
+        //该函数返回已经准备好的描述符事件数目
+        int ret = epoll_wait(epollfd,events,EPOLLEVENTS,-1);
+        //处理接收到的连接
+        char buf[10000];
+        handle_events(epollfd,events,ret,sockfd,buf);
     }
-    return 0;
+//    for (;;) {
+//        //不间断接收HTTP请求并处理，这里使用单线程，在实际情况下考虑到效率一般多线程
+//        newfd = accept(sockfd, NULL, NULL);
+//        serve(newfd);
+//        close(newfd);
+//    }
+//    return 0;
+
 }
